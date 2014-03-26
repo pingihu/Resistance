c36f99bdedaa8ea036028785157c4c3b116be4d0
Ping Hu <pingihu@gmail.com>
2014-03-25 23:08:05 -0700

    Finished the game (functionally). Made the colors prettier. Instructions improved. I wonder if the random-ing of mafia vs. good guys is random enough. --ping

--- resistance.py
+++ resistance.py
@@ -2,6 +2,7 @@ from graphics import *
 import random
 import time
 import math
+import random
 
 good = []
 bad = []
@@ -25,11 +26,10 @@ def assign(players):
     
     for i in range(0,num_bad):
         characters[players[i]] = ["Mafia"]
-        print "now looking at: %s" % (players[i])
-        bad_guys += players[i]
+        bad_guys.append(players[i])
     for j in range(num_bad,num_players):
         characters[players[j]] = ["Good Guy"]
-        good_guys += players[j]
+        good_guys.append(players[j])
         
 
     for p in characters.keys():
@@ -43,12 +43,18 @@ def assign(players):
     random.shuffle(bad_guys)
     random.shuffle(good_guys)
 
-    characters[bad_guys[0]][0] = "Snape (Mafia)"
-    characters[good_guys[0]][0] = "Kingpin (Good Guy)"
+    does_snape_exist = (random.randint(1,100) % 2 == 0)
+        
 
-    good = good_guys
-    bad = bad_guys
-    print bad
+    if does_snape_exist:
+        characters[bad_guys[0]][0] = "Snape (Mafia)"
+        characters[good_guys[0]][0] = "Kingpin (Good Guy)"
+        characters[good_guys[0]].append(bad_guys) #gives kingpin mafia info
+        characters[good_guys[0]].append(bad_guys[0]) #gives mafia snape info
+    else:
+        characters[good_guys[0]][0] = "Kingpin (Good Guy)"
+        characters[good_guys[0]].append(bad_guys) #gives kingpin mafia info
+        characters[good_guys[0]].append("No Snape") #gives mafia snape info
 
     return characters
 
@@ -66,9 +72,11 @@ def getPlayers(win):
 
 
     player_list = []
+
+    
     
     while (x != ""):
-        player_list += x
+        player_list.append(x)
         player_name= Entry(Point(140*ctr,180+(30*y_pos)), 20)
         player_name.setText(x)
         player_name.draw(win)
@@ -79,8 +87,8 @@ def getPlayers(win):
         x = raw_input('Enter player names (hit enter if done entering): ')
 
     
-
     return player_list
+
 
 """
 Returns a string representing your teammates
@@ -104,12 +112,17 @@ def passComputer(win, players):
     win = GraphWin("The Resistance!", 700, 500)
     win.setBackground('black')
 
+    orig_players = []
+    orig_players += players
+    
     character_dict = assign(players)
 
-    for i in players:
-        curr_msg = "Pass the computer to %s. %s, click anywhere to view your character." % (i, i)
+    for i in orig_players:
+        curr_msg = "Pass the computer to %s. %s, click anywhere to view your character. \n WARNING: You will have 15 seconds ONLY to view your character information." % (i, i)
         curr_text = Text(Point(350,100), curr_msg)
+        curr_text.setSize(14)
         curr_text.setTextColor('red')
+        
         curr_text.draw(win)
         #wait for the user to click and see their character
         win.getMouse()
@@ -122,31 +135,37 @@ def passComputer(win, players):
 
         teammates = info[1]
         teammates = filter(lambda(x): x!=i, teammates)
-        team_string = ' '.join(str(x) for x in teammates)
-
-        print team_string
+        team_string = '\n'.join(str(x) for x in teammates)
         
-        character_msg = "You are %s!  \nTeammates: %s" % (character_name, team_string)
+        character_msg = "You are %s!  \nTeammates: \n%s" % (character_name, team_string)
         character_msg_good = ""
         character_msg_bad = ""
+        snape_msg = ""
 
         if "Mafia" in character_name:
-            win.setBackground('red')
+            win.setBackground('dark red')
         elif "Good" in character_name:
-            win.setBackground('blue')
+            win.setBackground('cornflower blue')
             if "Kingpin" in character_name:
                 character_msg = "You are Kingpin!\n"
-                character_msg_good = "Good guys:%s" % (team_string)
-                character_msg_bad = "Mafia:%s" % ('\n'.join(str(e) for e in bad))
+                character_msg_good = "Good guys: \n%s" % (team_string)
+                character_msg_bad = "Mafia: \n%s" % ('\n'.join(str(e) for e in info[2]))
+                snape_msg = "Snape:\n%s" % (info[3]) 
 
         screen_text = Text(Point(350,100), character_msg)
         screen_text.setSize(22)
         KP_text1 = Text(Point(200, 200), character_msg_good)
+        KP_text1.setSize(22)
         KP_text2 = Text(Point(400, 200), character_msg_bad)
+        KP_text2.setSize(22)
+        KP_snape = Text(Point(300, 450), snape_msg)
+        KP_snape.setSize(22)
+        
 
         screen_text.draw(win)
         KP_text1.draw(win)
         KP_text2.draw(win)
+        KP_snape.draw(win)
 
 
         
@@ -163,7 +182,7 @@ def passComputer(win, players):
                 timer.undraw()
                 diff = int(cont - begin)
                 ctr += 1
-                timer = Text(Point(630,20),ctr)
+                timer = Text(Point(630,20),(16 - ctr))
                 timer.setSize(20)
                 timer.draw(win)
             cont = time.time()
@@ -196,19 +215,14 @@ def main():
     titleText.setFace('courier')
     titleText.draw(win)
 
-    ex_out = Rectangle(Point(670,2), Point(705, 30))
-    ex_out.setFill('grey')
-    ex_out.draw(win)
-    x = Text(ex_out.getCenter(), "X")
-    x.setSize(30)
-    x.setTextColor('black')
-    x.setStyle('bold')
-    x.draw(win)
+    instructions = Text(Point(350, 450), 'If you are playing with people who have the same names,\nplease add initials (or use different names)')
+    instructions.setTextColor('red')
+    instructions.setFace('courier')
+    instructions.draw(win)
 
     #the following function will write all the player's names to
     #the game screen, then return this list of players.
     player_list = getPlayers(win)
-    print player_list
 
     ready_text = Text(Point(360, 170), "Click anywhere to start!")
     ready_text.setSize(35)
